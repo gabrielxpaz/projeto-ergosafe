5e913c8d1560ce84714e0cc928e551a357893aba
unisanta <unisanta@santacecilia.br>
2025-05-08 19:47:41 -0300

    Resolvido bug do Regex da senha

--- backend/helpers/validationHelper.js
+++ backend/helpers/validationHelper.js
@@ -1,6 +1,6 @@
 exports.validateUserInput = ({ name, email, password, role_id }) => {
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+  const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
 
   if (!name || !email || !password || !role_id) {
     return "Todos os campos são obrigatórios.";
@@ -9,7 +9,7 @@ exports.validateUserInput = ({ name, email, password, role_id }) => {
     return "Email inválido.";
   }
   if (!passwordRegex.test(password)) {
-    return "A senha deve ter pelo menos 8 caracteres, incluindo uma letra e um número.";
+    return "A Senha é fraca demais (1 letra maiuscula, 1 letra minuscula, 1 numero e 1 caractere especial)";
   }
   return null; // Nenhum erro
 };
