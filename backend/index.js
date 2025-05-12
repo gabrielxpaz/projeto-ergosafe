17cad4b704a2d95c2e9c3b69d82fa793946a3c85
unisanta <unisanta@santacecilia.br>
2025-05-12 20:04:12 -0300

    Implementado handlebars

--- backend/index.js
+++ backend/index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const db = require("./db/db.js");
 const app = express();
 const cors = require("cors");
+const handlebars = require("express-handlebars");
 const authRoutes = require("./routes/authRoutes.js");
 
-//Middlewares
+// Configuração do Handlebars
+app.engine("handlebars", handlebars.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./views");
+
+// Middlewares
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//Roteamento
+// Roteamento
 app.use("/api/auth", authRoutes);
+app.get("/", (req, res) => {
+  res.render("home"); // Renderiza a view "home.handlebars"
+});
 
-//Conexão banco
+// Conexão banco
 db.sync().then(() => {
   app.listen(5000, () => {
     console.log("Banco de dados conectado!");
